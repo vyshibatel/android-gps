f8d237ee4080b51ea581483f36be14d0292f85e0
vyshibatel <34483923+vyshibatel@users.noreply.github.com>
2025-09-25 04:52:06 -0400

    Initial commit

--- app/src/main/java/io/github/jqssun/gpssetter/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/io/github/jqssun/gpssetter/ui/viewmodel/MainViewModel.kt
@@ -183,7 +183,11 @@ class MainViewModel @Inject constructor(
             mkdirs()
         }
         downloadFile = File(downloadFolder, fileName)
-        context.registerReceiver(downloadStateReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        context.registerReceiver(
+            downloadStateReceiver,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+            Context.RECEIVER_EXPORTED // Просто добавьте этот флаг
+        )
         context.contentResolver.registerContentObserver(Uri.parse("content://downloads/my_downloads"), true, downloadObserver)
         requestId = DownloadManager.Request(Uri.parse(url)).apply {
             setDescription(context.getString(R.string.download_manager_description))
